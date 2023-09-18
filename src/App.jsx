fa54167732a9af6fa939f8d46a01ed1d3906bd2a
Typhann <72917604+Typhann@users.noreply.github.com>
2023-09-18 17:22:01 +0200

    test font-size

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,43 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hotelfinder</h1>
+      <div className="card">
+        <h1 className="h1-c1">Grand Hyatt Resort</h1>
+        <h2 className="h2-c1">Playa del Carmen, Mexico</h2>
+        <h3 className="h3-c1">Hotelfinder</h3>
+        <p className="p-c1">
+          Offering 3 ocean-front outdoor pools and a spa and wellness center
+          with spectacular architecture resembling a Cenote, Grand Hyatt Playa
+          del Carmen Resort is located in Mamita’s Beach. The design concept has
+          crafted natural elements, artwork, textures and light.
+        </p>
+      </div>
+      <div className="card">
+        <h1 className="h1-c2">Grand Hyatt Resort</h1>
+        <h2 className="h2-c2">Playa del Carmen, Mexico</h2>
+        <h3 className="h3-c2">
+          Experience World-class Service at Grand Hyatt Playa del Carmen Resort
+        </h3>
+        <p className="p-c2">
+          Offering 3 ocean-front outdoor pools and a spa and wellness center
+          with spectacular architecture resembling a Cenote, Grand Hyatt Playa
+          del Carmen Resort is located in Mamita’s Beach. The design concept has
+          crafted natural elements, artwork, textures and light.
+        </p>
+      </div>
+      <div className="card">
+        <h1 className="h1-c3">Grand Hyatt Resort</h1>
+        <h2 className="h2-c3">Playa del Carmen, Mexico</h2>
+        <h3 className="h3-c3">
+          Experience World-class Service at Grand Hyatt Playa del Carmen Resort
+        </h3>
+        <p className="p-c3">
+          Offering 3 ocean-front outdoor pools and a spa and wellness center
+          with spectacular architecture resembling a Cenote, Grand Hyatt Playa
+          del Carmen Resort is located in Mamita’s Beach. The design concept has
+          crafted natural elements, artwork, textures and light.
+        </p>
+      </div>
     </>
   );
 }
