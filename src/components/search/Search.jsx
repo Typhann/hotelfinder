45113c4bae6254498ceb411b65b2d30ae5a96293
Typhann <72917604+Typhann@users.noreply.github.com>
2023-09-22 18:55:11 +0200

    add search btn

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,16 +3,19 @@ import { FaLocationArrow, FaCalendarAlt, FaUser } from "react-icons/fa";
 export default function Search() {
   return (
     <div className="search-container">
-      <p>
-        <FaLocationArrow /> <strong>Where</strong>
-      </p>
-      <p>
-        <FaCalendarAlt /> <strong>When</strong>
-      </p>
-      <p>
-        <FaUser />
-        <strong>Who</strong>
-      </p>
+      <div className="search-options">
+        <p>
+          <FaLocationArrow /> <strong>Where</strong>
+        </p>
+        <p>
+          <FaCalendarAlt /> <strong>When</strong>
+        </p>
+        <p>
+          <FaUser />
+          <strong>Who</strong>
+        </p>
+      </div>
+      <button>Search</button>
     </div>
   );
 }
