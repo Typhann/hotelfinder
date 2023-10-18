774d3a8f5fb09723f7878b201b495ef5484b20a1
Typhann <72917604+Typhann@users.noreply.github.com>
2023-10-18 19:32:40 +0200

    put filter and profile inte MUI drawer components

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,19 +1,10 @@
 import { useState } from "react";
 import Slider from "@mui/material/Slider";
 import { FaSlidersH, FaWindowClose, FaStar, FaRegStar } from "react-icons/fa";
+import Drawer from "@mui/material/Drawer";
 
 export default function FilterButtons() {
-  {
-    /*Function for trigger filter section*/
-  }
-  const [isClosed, setIsClosed] = useState("none");
-  const openOverlay = () => {
-    setIsClosed("block");
-  };
-  const closeOverlay = () => {
-    setIsClosed("none");
-  };
-
+  const [openDrawer, setOpenDrawer] = useState(false);
   return (
     <>
       {/*Buttons for trigger filter section*/}
@@ -24,80 +15,87 @@ export default function FilterButtons() {
           <option value="LowToHigh">Price - Low To High</option>
           <option value="HighToLow">Price - High To Low</option>
         </select>
-        <button onClick={openOverlay} className="button">
+        <button onClick={() => setOpenDrawer(true)} className="button">
           <FaSlidersH /> Filter
         </button>
       </div>
 
       {/*Filter section, hidden on defeault*/}
-      <div className="overlay filter-section" style={{ display: isClosed }}>
-        <button
-          onClick={closeOverlay}
-          className="close-filter-overlay"
-          style={{ border: "none", fontSize: "18px", background: "none" }}
-        >
-          <FaWindowClose />
-        </button>
-        <h2>Search</h2>
-        <input placeholder="search hotel ...." />
-        <h2>Filter by</h2>
-        <h3>Popular filters</h3>
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <div className="overlay filter-section slide">
+          <button
+            onClick={() => setOpenDrawer(false)}
+            className="close-filter-overlay"
+            style={{ border: "none", fontSize: "18px", background: "none" }}
+          >
+            <FaWindowClose />
+          </button>
+          <h2>Search</h2>
+          <input placeholder="search hotel ...." />
+          <h2>Filter by</h2>
+          <h3>Popular filters</h3>
 
-        <div className="popular-filters">
-          <ul>
-            <li>
-              <input type="checkbox" value="Meals" name="HotelPreference" />{" "}
-              Meals included
-            </li>
-            <li>
-              <input type="checkbox" value="Family" name="HotelPreference" />{" "}
-              Family friendly
-            </li>
-            <li>
-              <input type="checkbox" value="Beach" name="hotelpreference" />{" "}
-              Beach
-            </li>
-            <li>
-              <input type="checkbox" value="Pool" name="hotelpreference" /> Pool
-            </li>
-            <li>
-              <input type="checkbox" value="WiFi" name="hotelpreference" /> WiFi
-            </li>
-          </ul>
-        </div>
+          <div className="popular-filters">
+            <ul>
+              <li>
+                <input type="checkbox" value="Meals" name="HotelPreference" />{" "}
+                Meals included
+              </li>
+              <li>
+                <input type="checkbox" value="Family" name="HotelPreference" />{" "}
+                Family friendly
+              </li>
+              <li>
+                <input type="checkbox" value="Beach" name="hotelpreference" />{" "}
+                Beach
+              </li>
+              <li>
+                <input type="checkbox" value="Pool" name="hotelpreference" />{" "}
+                Pool
+              </li>
+              <li>
+                <input type="checkbox" value="WiFi" name="hotelpreference" />{" "}
+                WiFi
+              </li>
+            </ul>
+          </div>
 
-        <h3>Price per night</h3>
-        {/*slider from @mui/material/Slider*/}
-        <Slider max={1000} min={50} />
-        <h3>Guest rating</h3>
-        <div className="quest-rating-filter">
-          <ul>
-            <li>
-              <input type="checkbox" value="Wonderful" name="Wonderful" />{" "}
-              Wonderful
-            </li>
-            <li>
-              <input type="checkbox" value="Very Good" name="Very Good" /> Very
-              Good
-            </li>
-            <li>
-              <input type="checkbox" value="Good" name="Good" /> Good
-            </li>
-          </ul>
-        </div>
+          <h3>Price per night</h3>
+          {/*slider from @mui/material/Slider*/}
+          <Slider max={1000} min={50} />
+          <h3>Guest rating</h3>
+          <div className="quest-rating-filter">
+            <ul>
+              <li>
+                <input type="checkbox" value="Wonderful" name="Wonderful" />{" "}
+                Wonderful
+              </li>
+              <li>
+                <input type="checkbox" value="Very Good" name="Very Good" />{" "}
+                Very Good
+              </li>
+              <li>
+                <input type="checkbox" value="Good" name="Good" /> Good
+              </li>
+            </ul>
+          </div>
 
-        <h3>Hotel star rating</h3>
-        <div className="star-rating-filter">
-          <FaStar />
-          <FaStar />
-          <FaStar />
-          <FaRegStar />
-          <FaRegStar />
+          <h3>Hotel star rating</h3>
+          <div className="star-rating-filter">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar />
+            <FaRegStar />
+          </div>
+          <button
+            onClick={() => setOpenDrawer(false)}
+            className="button purple-button"
+          >
+            Apply filter
+          </button>
         </div>
-        <button onClick={closeOverlay} className="button purple-button">
-          Apply filter
-        </button>
-      </div>
+      </Drawer>
     </>
   );
 }
--- src/components/header/Profile.jsx
+++ src/components/header/Profile.jsx
@@ -1,206 +1,217 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import database from "../../../database.json";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthenticatedContext from "../../context/AuthenticatedContext";
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
 
 import {
   FaEdit,
   FaUserCircle,
   FaAngleDoubleRight,
   FaWindowClose,
-  FaStar, 
+  FaStar,
   FaRegStar,
-  FaCheck
+  FaCheck,
 } from "react-icons/fa";
 
 export default function Profile() {
   const { setAuthenticated } = useContext(AuthenticatedContext);
-  const navigate =  useNavigate()
+  const navigate = useNavigate();
   const user = database.users;
-  
-  {
-    /*Function for trigger profile section*/
-  }
-  const [isClosed, setIsClosed] = useState("none");
 
-  const openOverlay = () => {
-    setIsClosed("block");
-  };
-  const closeOverlay = () => {
-    setIsClosed("none");
-  };
+  const [openDrawer, setOpenDrawer] = useState(false);
 
   return (
     <>
-      <div className="profile-icon-header" onClick={openOverlay}>
+      <div className="profile-icon-header" onClick={() => setOpenDrawer(true)}>
         <FaUserCircle />
       </div>
-      <div className="overlay profile" style={{ display: isClosed }}>
-        <button
-          onClick={closeOverlay}
-          className="close-filter-overlay"
-          style={{ border: "none", fontSize: "18px", background: "none" }}
-        >
-          <FaWindowClose />
-        </button>
-
-        <div className="my-profile-header">
-          <h2>My profile</h2>
-          <div className="user-icon">
-            <FaUserCircle />
-          </div>
-        </div>
-
-        <div className="profile-links">
-          <ul className="profile-link-list">
-            <li>
-              <Link onClick={closeOverlay} to="Favorites">
-                Favorites <FaAngleDoubleRight />
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                Reviews <FaAngleDoubleRight />
-              </Link>
-            </li>
-          </ul>
-
-          <ul className="profile-link-list">
-            <li>
-              <Link to="/">
-                Bookings <FaAngleDoubleRight />
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                Previous stays <FaAngleDoubleRight />
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div>
-          <h3>Profile information</h3>
-          <div className="bold profile-information">
-            <div className="profile-name">
-              <p>
-                <span>Name:</span><br /> 
-                {user[0].name}
-              </p>
-            </div>
-
-            <div className="profile-adress">
-              <p>
-                <span>Adress:</span><br /> 
-                {user[0].address}{" "}
-              </p>
-              <p>
-                <span>zip-code:</span><br /> 
-                {user[0].zip_code}{" "}
-              </p>
-            </div>
-
-            <div className="profile-contact">
-              <p>
-                <span>Phone number:</span><br /> 
-                {user[0].phone}{" "}
-              </p>
-              <p>
-                <span>Email address:</span><br /> 
-                {user[0].email}{" "}
-              </p>
-            </div>
-
-          </div>
-          <button className="edit-profile-button">
-            <FaEdit /> Edit profile
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <div className="overlay profile slide">
+          <button
+            onClick={() => setOpenDrawer(false)}
+            className="close-filter-overlay"
+            style={{ border: "none", fontSize: "18px", background: "none" }}
+          >
+            <FaWindowClose />
           </button>
-        </div>
 
-        <div className="upcoming_stay">
-          <h3>Your upcoming stay</h3>
-
-          <div className="inline">
-            <h4 className="hotel_title">Grand Hyatt Resort</h4>
-            <div className="rates">
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaRegStar />
-            <FaRegStar />
+          <div className="my-profile-header">
+            <h2>My profile</h2>
+            <div className="user-icon">
+              <FaUserCircle />
             </div>
           </div>
 
-          <div className="booking-info">
-            <p className="bold font-small"><span>Airport transfer <FaCheck/></span></p>
-            <p className="bold font-small"><span>Regusted time of check-in:</span></p>
-            <p>10:00 Wed, Now 8 2023</p>
+          <div className="profile-links">
+            <ul className="profile-link-list">
+              <li>
+                <Link to="Favorites">
+                  Favorites <FaAngleDoubleRight />
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  Reviews <FaAngleDoubleRight />
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="profile-link-list">
+              <li>
+                <Link to="/">
+                  Bookings <FaAngleDoubleRight />
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  Previous stays <FaAngleDoubleRight />
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h3>Profile information</h3>
+            <div className="bold profile-information">
+              <div className="profile-name">
+                <p>
+                  <span>Name:</span>
+                  <br />
+                  {user[0].name}
+                </p>
+              </div>
+
+              <div className="profile-adress">
+                <p>
+                  <span>Adress:</span>
+                  <br />
+                  {user[0].address}{" "}
+                </p>
+                <p>
+                  <span>zip-code:</span>
+                  <br />
+                  {user[0].zip_code}{" "}
+                </p>
+              </div>
+
+              <div className="profile-contact">
+                <p>
+                  <span>Phone number:</span>
+                  <br />
+                  {user[0].phone}{" "}
+                </p>
+                <p>
+                  <span>Email address:</span>
+                  <br />
+                  {user[0].email}{" "}
+                </p>
+              </div>
+            </div>
             <button className="edit-profile-button">
-              <FaEdit /> Manage booking
+              <FaEdit /> Edit profile
             </button>
           </div>
-        </div>
 
-        <div className="recently_watched">
-          <h3>Recently watched</h3>
-   
-        <div className="recently_watched-hotel">
-          <div className="inline">
-            <h4 className="hotel_title">Ski Lodge</h4>
-            <div className="rates">
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaRegStar />
+          <div className="upcoming_stay">
+            <h3>Your upcoming stay</h3>
+
+            <div className="inline">
+              <h4 className="hotel_title">Grand Hyatt Resort</h4>
+              <div className="rates">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+            </div>
+
+            <div className="booking-info">
+              <p className="bold font-small">
+                <span>
+                  Airport transfer <FaCheck />
+                </span>
+              </p>
+              <p className="bold font-small">
+                <span>Requested time of check-in:</span>
+              </p>
+              <p>10:00 Wed, Now 8 2023</p>
+              <button className="edit-profile-button">
+                <FaEdit /> Manage booking
+              </button>
             </div>
           </div>
-          <div className="inline">
-          <p>Winter Sports Haven</p>
-          <p className="bold"><span>$229</span></p>
-          </div>
-        </div>
 
-        <div className="recently_watched-hotel">
-          <div className="inline">
-            <h4 className="hotel_title">Seaside Resort</h4>
-            <div className="rates">
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaStar />
-            <FaStar />
+          <div className="recently_watched">
+            <h3>Recently watched</h3>
+
+            <div className="recently_watched-hotel">
+              <div className="inline">
+                <h4 className="hotel_title">Ski Lodge</h4>
+                <div className="rates">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaRegStar />
+                </div>
+              </div>
+              <div className="inline">
+                <p>Winter Sports Haven</p>
+                <p className="bold">
+                  <span>$229</span>
+                </p>
+              </div>
+            </div>
+
+            <div className="recently_watched-hotel">
+              <div className="inline">
+                <h4 className="hotel_title">Seaside Resort</h4>
+                <div className="rates">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+              <div className="inline">
+                <p>Beachfront Paradise</p>
+                <p className="bold">
+                  <span>$360</span>
+                </p>
+              </div>
+            </div>
+
+            <div className="recently_watched-hotel">
+              <div className="inline">
+                <h4 className="hotel_title">Mountain Lodge</h4>
+                <div className="rates">
+                  <FaStar />
+                  <FaStar />
+                  <FaRegStar />
+                  <FaRegStar />
+                  <FaRegStar />
+                </div>
+              </div>
+              <div className="inline">
+                <p>Nature Lover's Retreat </p>
+                <p className="bold">
+                  <span>$145</span>
+                </p>
+              </div>
             </div>
           </div>
-          <div className="inline">
-          <p>Beachfront Paradise</p>
-          <p className="bold"><span>$360</span></p>
-          </div>
-        </div>
 
-        <div className="recently_watched-hotel">
-          <div className="inline">
-            <h4 className="hotel_title">Mountain Lodge</h4>
-            <div className="rates">
-            <FaStar />
-            <FaStar />
-            <FaRegStar />
-            <FaRegStar />
-            <FaRegStar />
-            </div>
-          </div>
-          <div className="inline">
-          <p>Nature Lover's Retreat </p>
-          <p className="bold"><span>$145</span></p>
-          </div>
+          <button
+            onClick={() => (setAuthenticated(false), navigate("/"))}
+            className="button purple-button"
+          >
+            Log out
+          </button>
         </div>
-          
-        </div>
-
-        <button  onClick={ () => (setAuthenticated(false), navigate("/"))} className="button purple-button">Log out</button>
-      </div>
+      </Drawer>
     </>
   );
 }
