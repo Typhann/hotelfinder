a5382b98cf4c1ced0436c2e71f10996a4894e399
Typhann <72917604+Typhann@users.noreply.github.com>
2023-10-19 15:40:07 +0200

    minor styling to favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,7 @@
 import HotelCard from "../components/HotelCard";
 import Filter from "../components/Filter";
-import database from "../../database.json"
-import {
-  FaShareAlt,
-} from "react-icons/fa";
+import database from "../../database.json";
+import { FaShareAlt } from "react-icons/fa";
 
 export default function Favorite() {
   // show first 8 objects in JSON to visualize favorite items
@@ -11,32 +9,29 @@ export default function Favorite() {
 
   return (
     <>
-      <section className="favorit-section">
-        
+      <section className="favorite-section">
         <div className="share-favorites">
-        <h1>Favorite page</h1>
+          <h1>My Favorites</h1>
           <div className="share-list">
             <FaShareAlt />
             <p>share list</p>
           </div>
         </div>
-        
+
         <div className="filter-buttons">
-        <Filter />
+          <Filter />
         </div>
 
         <div className={`hotels-container grid`}>
-          
           {showHotels.map((hotel) => (
-          <HotelCard
-            key={hotel.id}
-            name={hotel.name}
-            address={hotel.address}
-            price={hotel.price_per_night}
-            id={hotel.id}
-          />
-        ))}
-       
+            <HotelCard
+              key={hotel.id}
+              name={hotel.name}
+              address={hotel.address}
+              price={hotel.price_per_night}
+              id={hotel.id}
+            />
+          ))}
         </div>
       </section>
     </>
