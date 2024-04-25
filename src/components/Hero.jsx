3481bc1fc1e992dc16c0bb251785f2d1cc1439a8
Typhann <72917604+Typhann@users.noreply.github.com>
2024-04-25 12:55:39 +0200

    add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div className="hero">
+      <button className="button">Explore</button>
+      <h2>Book Hotels With Ease</h2>
+    </div>
+  );
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header/Header";
 import Footer from "./Footer";
+import Hero from "../components/Hero";
 
 // Component for site layout
 export default function Layout() {
@@ -8,6 +9,7 @@ export default function Layout() {
     <>
       <div className="site-wrapper">
         <Header />
+        <Hero />
         <main>
           {/* Outlet changes depending on the route */}
           <Outlet />
